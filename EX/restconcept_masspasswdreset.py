91af1544a349511809112b5202e2a3677109f694
LinuxFlyGuy <vagrant83@gmail.com>
2018-02-01 19:29:27 -0500

    password reset beta finished.

--- EX/restconcept_masspasswdreset.py
+++ EX/restconcept_masspasswdreset.py
@@ -12,9 +12,11 @@ from packetmasterEX_rest import PacketmasterEX
 
 def usage():
     print """Usage:
-             -r, --random FILE   Provide a JSON formatted text file of device IPs.  All users on provided devices
-                            will have current password replaced with a random password.  A file of usernames
-                            and correspnding new passwords will be generated.
+             -r, --random FILE   Provide a JSON formatted text file of device IPs and admin credentials.
+                                 All users on provided devices will have current password replaced with
+                                 a random password.  A file of usernames and correspnding new passwords
+                                 will be created on this device.  This will NOT change the provided
+                                 admin user's password to insure proper execution (see -a option).
                             Format:
                             {
                             "packetmaster": [
@@ -22,8 +24,10 @@ def usage():
                                             {"ip": "192.168.0.3", "admin": "username", "pass": "passwd"}
                                             ]
                             }
+
              -f, --fromfile FILE   Provide a JSON formatted text file of device IPs, usernames, and new passwords.
-                              Current passwords will be replaced with passwords provided in the file.
+                                   Current passwords will be replaced with passwords provided in the file.  This will
+                                   NOT change the provided admin user's password to insure proper execution (see -a option).
                               Format:
                               {
                               "packetmaster": [
@@ -34,7 +38,19 @@ def usage():
                                 {"username": "user1", "password": "passwd1"},
                                 {"username": "user2", "password": "passwd2"}
                               ]
-                              }"""
+                              }
+
+            -a, --admin FILE   Provide a JSON formatted text file of device IPs and admin credentials.
+                               This option will replace ONLY the provided device admin's password with
+                               a randomly generated password.  A file containing the admin's new password
+                               will be created on this device.
+                             Format:
+                             {
+                             "packetmaster": [
+                                             {"ip": "192.168.0.2", "admin": "username", "pass": "passwd"},
+                                             {"ip": "192.168.0.3", "admin": "username", "pass": "passwd"}
+                                             ]
+                             }"""
 
 def rand_reset(ip, admin_username, admin_password):
     ex = PacketmasterEX(ip, admin_username, admin_password)
@@ -44,13 +60,22 @@ def rand_reset(ip, admin_username, admin_password):
         results["conn"] = True
     else:
         results["conn"] = False
+        return results
     user_list = ex.get_users()
     data = json.loads(user_list)
     for user in data:
-        passwd = rand_generator()
-        changes[user] = passwd
-        reset = ex.mod_user(user, user, data[user]["accesslevel"], passwd, data[user]["description"], data[user]["radius"])
-    results["changes"] = changes
+        if user != admin_username:
+            passwd = rand_generator()
+            changes[user + "@" + ip] = {"device": ip,
+                                        "username": user,
+                                        "password": passwd}
+            reset = ex.mod_user(data[user]["username"],
+                                data[user]["username"],
+                                data[user]["accesslevel"],
+                                passwd,
+                                data[user]["description"],
+                                data[user]["radius"])
+    results["users"] = changes
     return results
 
 def file_reset(ip, admin_username, admin_password, user_list):
@@ -60,8 +85,46 @@ def file_reset(ip, admin_username, admin_password, user_list):
         results["conn"] = True
     else:
         results["conn"] = False
+        return results
+    to_change = []
+    cur_users = ex.get_users()
+    data = json.loads(cur_users)
+    for user in cur_users:
+        to_change.append(user)
     for user in user_list:
-        print user["username"], user["password"]
+        if user["username"] in to_change and user["username"] != admin_username:
+            key = user["username"]
+            reset = ex.mod_user(data[key]["username"],
+                                data[key]["username"],
+                                data[key]["accesslevel"],
+                                user["password"],
+                                data[key]["description"],
+                                data[key]["radius"])
+    return results
+
+def admin_reset(ip, admin_username, admin_password):
+    ex = PacketmasterEX(ip, admin_username, admin_password)
+    results = {}
+    if ex.conn_test() == "Connection established":
+        results["conn"] = True
+    else:
+        results["conn"] = False
+        return results
+    user_list = ex.get_users()
+    data = json.loads(user_list)
+    passwd = rand_generator()
+    for user in data:
+        if user == admin_username
+        changes[admin_username + "@" + ip] = {"device": ip,
+                                              "username": user,
+                                              "password": passwd}
+        reset = ex.mod_user(data[user]["username"],
+                            data[user]["username"],
+                            data[user]["accesslevel"],
+                            passwd,
+                            data[user]["description"],
+                            data[user]["radius"])
+    results["users"] = changes
     return results
 
 def rand_generator(size=16, chars=string.ascii_uppercase + string.ascii_lowercase + string.digits):
@@ -87,13 +150,17 @@ if __name__ == '__main__':
                     success.append(ip)
                 else:
                     failed.append(ip)
-                for pair in run["changes"]:
-                    user_list.append(pair)
+                try:
+                    for entry in run["users"]:
+                        user_list.append(entry)
+                except:
+                    pass
         output_file["Successfully Changed"] = success
         output_file["Failed to connect"] = failed
         output_file["New User Passwords"] = user_list
-        with open('password_reset.json', 'w') as output:
-            output.write(output_file)
+        output = json.dumps(output_file, indent=4)
+        with open('random_password_reset.json', 'w') as o:
+            o.write(output)
     elif len(sys.argv) > 1 and str(sys.argv[1]) == '-f' or len(sys.argv) > 1 and str(sys.argv[1]) == '--fromfile':
         filename = sys.argv[2]
         device_list = []
@@ -114,7 +181,36 @@ if __name__ == '__main__':
                     failed.append(ip)
         output_file["Successfully Changed"] = success
         output_file["Failed to connect"] = failed
-        with open('password_reset.json', 'w') as output:
-            output.write(output_file)
+        output = json.dumps(output_file, indent=4)
+        with open('password_reset.json', 'w') as o:
+            o.write(output)
+    elif len(sys.argv) > 1 and str(sys.argv[1]) == '-a' or len(sys.argv) > 1 and str(sys.argv[1]) == '--admin':
+        filename = sys.argv[2]
+        device_list = []
+        success = []
+        failed = []
+        output_file = {}
+        with open(filename) as f:
+            device_file = json.load(f)
+            for item in device_file["packetmaster"]:
+                ip = item["ip"]
+                user = item["admin"]
+                passwd = item["passwd"]
+                run = rand_reset(ip, user, passwd)
+                if run["conn"] == True:
+                    success.append(ip)
+                else:
+                    failed.append(ip)
+                try:
+                    for entry in run["users"]:
+                        user_list.append(entry)
+                except:
+                    pass
+        output_file["Successfully Changed"] = success
+        output_file["Failed to connect"] = failed
+        output_file["New User Passwords"] = user_list
+        output = json.dumps(output_file, indent=4)
+        with open('admin_reset.json', 'w') as o:
+            o.write(output)
     else:
         usage()
