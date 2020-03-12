721e01098ea0fe256edc54a8510293d332f1e054
TechnoSavage <technosavage@gmail.com>
2020-03-12 15:52:31 -0400

    Added MAC address validation. Improved docstrings.

--- EX/pm_input_check.py
+++ EX/pm_input_check.py
@@ -7,12 +7,27 @@ import re
 from datetime import datetime
 
 def mac(test):
-    '''Check for valid MAC address and return if found.'''
-    mac = re.findall("pattern", test)
-    return None
+    '''Check for valid MAC address.
+       
+       :param test: A string, string to test if valid MAC address format.
+       :returns: A string, if input contains a regular expression match the first match is returned.
+       :returns: An int, if input contains no valid MAC address format 0 is returned.
+       :raises: TypeError: if regular expression match cannot be run on test variable.'''
+    try:
+        mac = re.findall("((?:[\da-fA-F]{2}[:\-]){5}[\da-fA-F]{2})", test)
+        if len(mac) == 1:
+            return mac[0]
+    except TypeError as reason:
+        log_event(reason)
+        return 0
+    return 0
 
 def vlan(test):
-    '''Check for valid VLAN tag number.'''
+    '''Check for valid VLAN tag number.
+    
+       :param test: A string or int, input to test if valid VLAN number.
+       :returns: A bool, True is valid VLAN, False if not.
+       :raises: ValueError: if test variable cannot be converted to int.'''
     try:
         test = int(test)
         if test in range(0, 4097):
@@ -23,7 +38,11 @@ def vlan(test):
     return False
 
 def vlan_pri(test):
-    '''Check for valid VLAN priority number.'''
+    '''Check for valid VLAN priority number.
+    
+       :param test: A string or int, input to test for valid VLAN priority number.
+       :returns: A bool, True if valid; False if invalid.
+       :raises: ValueError: if test variable cannot be converted to int.'''
     try:
         test = int(test)
         if test in range(0, 8):
@@ -34,7 +53,12 @@ def vlan_pri(test):
     return False
 
 def ipv4(test):
-    '''Check for valid IPv4 address and return if found.'''
+    '''Check for valid IPv4 address and return if found.
+    
+       :param test: A string, string to test for valid IP address format.
+       :returns: A string, if input contains a regular expression match the first match is returned.
+       :returns: An int, if input contains no valid IP address format 0 is returned.
+       :raises: TypeError: if regular expression match cannot be run on test variable.'''
     try:
         ipv4_address = re.findall('\A(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$', test)
         if len(ipv4_address) == 1:
@@ -45,7 +69,12 @@ def ipv4(test):
     return 0
 
 def ipv4_mask(test):
-    '''Check for valid IPv4 address with optional decimal subnet mask.'''
+    '''Check for valid IPv4 address with optional decimal subnet mask.
+       
+       :param test: A string, string to test for valid IP address and Subnet mask/CIDR format.
+       :returns: A string, if input contains a valid match the match is returned in proper format.
+       :returns: An int, if input contains no valid IP + subnet/CIDR format 0 is returned.
+       :raises: TypeError: if regular expression match cannot be run on test variable.'''
     try:
         ipv4_address = re.findall('(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)', test)
         if "/" in test and len(ipv4_address) == 1:
@@ -66,11 +95,18 @@ def ipv4_mask(test):
     return 0
 
 def ipv6(test):
-    '''Check for valid IPv6 address and return if found.'''
+    '''Check for valid IPv6 address and return if found.
+    
+       :param test: A string, string to test for IPv6 address format.
+       :returns: NoneType, this test is not yet implemented.'''
     return None
 
 def port(test):
-    '''Test if input is a valid port number: return "True" if valid and "False" if not.'''
+    '''Test if input is a valid TCP or UDP port number.
+
+       :param test: A string or int, input to check if valid TCP/UDP port number. 
+       :returns: A bool, True if valid; False if not.
+       :raises: ValueError: if test variable cannot be converted to int.'''
     try:
         test = int(test)
         if test in range(1, 65536):
@@ -81,7 +117,11 @@ def port(test):
     return False
 
 def pm_pri(test):
-    '''Test if input is valid for Packetmaster G1-G4 Rule Priority value.'''
+    '''Test if input is valid for Packetmaster G1-G4 Rule Priority value.
+    
+       :param test: A string or int, string or value to test for valid Packetmaster Rule Priority.
+       :returns: A bool, True if valid; False if invalid.
+       :raises: ValueError: if test variable cannot be converted to int.'''
     try:
         test = int(test)
         if test in range(0, 65536):
@@ -92,7 +132,11 @@ def pm_pri(test):
     return False
 
 def icmp_type(test):
-    '''Test if input is a valid ICMP Type value.'''
+    '''Test if input is a valid ICMP Type value.
+    
+       :param test: A string or int, string or value to test for valid ICMP type.
+       :returns: A bool, True if valid; False if invalid.
+       :raises: ValueError: if test variable cannot be converted to int.'''
     try:
         test = int(test)
         if test in range(0, 256):
@@ -103,7 +147,11 @@ def icmp_type(test):
     return False
 
 def icmp_code(test):
-    '''Test if input is a valid ICMP Code value.'''
+    '''Test if input is a valid ICMP Code value.
+    
+       :param test: A string or int, string or value to test for valid ICMP Code.
+       :returns: A bool, True if valid; False if invalid.
+       :raises: ValueError: if test variable cannot be converted to int.'''
     try:
         test = int(test)
         if test in range(0, 16):
@@ -114,11 +162,15 @@ def icmp_code(test):
     return False
 
 def ethertype(test):
-    '''Test if input is a valid Ethertype.'''
+    '''Test if input is a valid Ethertype.
+    
+       :param test: A string, string to test for valid Ethertype.'''
     pass
 
 def log_event(reason):
-    '''Write errors to file if logging is enabled.'''
+    '''Write errors to file if logging is enabled.
+    
+       :param reason: A string, message to be written to file.'''
     #Change logging to True to write errors to log file specified under log_location variable
     logging = False
     #Alter log_location to reflect desired file path and name
