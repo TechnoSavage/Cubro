322bd7dd0ff53bf9d6fd15e471d561753b51b436
TechnoSavage <technosavage@gmail.com>
2022-01-13 13:16:54 -0500

    outlining script functions

--- Custos/c2_concept.py
+++ Custos/c2_concept.py
@@ -1,3 +1,21 @@
 #!/usr/bin/python
 
+"""A script to demonstrate concept of C2 detection in Custos"""
+
 import subprocess
+from datetime import datetime
+
+#API to export PCAP for previous 24 hours
+
+#Generate Zeek logs from PCAP
+#This line should be in local Zeek config
+
+#Create RITA database from Zeek logs
+
+#Define confidence score threshold
+
+confidence = 0.80
+
+#Iterate through RITA detections and create dict object for each entry that meets score threshold
+
+
