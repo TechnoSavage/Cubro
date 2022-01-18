41097b1e8a7eddf13d2afedd00c645d658839b97
TechnoSavage <technosavage@gmail.com>
2022-01-18 14:32:58 -0500

    .

--- Custos/c2_concept.py
+++ Custos/c2_concept.py
@@ -6,16 +6,28 @@ import subprocess
 from datetime import datetime
 
 #API to export PCAP for previous 24 hours
+pcap_file = 'athlab4.pcap'
+#Generate Zeek logs from PCAP, comment next line and uncomment the line after to use local Zeek config
+cmd = 'zeek -C -r ' + pcap_file + ' tcp_inactivity_timeout = 60min;'
+#cmd = 'zeek -C -r ' + pcap_file + ' local'
 
-#Generate Zeek logs from PCAP
-#This line should be in local Zeek config
+sp = subprocess.Popen(cmd, shell=True)
+rc = sp.wait()
+print(rc)
 
 #Create RITA database from Zeek logs
+rita_db_name = ''
+cmd = 'rita import ./*.log' + rita_db_name
 
-#Define confidence score threshold
 
-confidence = 0.80
+#Execute RITA detection mothods and create dict object for each entry that meets score threshold
 
-#Iterate through RITA detections and create dict object for each entry that meets score threshold
+confidence = 0.80  #Define confidence score threshold
+
+#RITA 'show-beacons' block
+
+#RITA 'show-beacons-fqdn' block
+
+
 
 
