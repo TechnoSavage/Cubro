d627c0d498fdcb2e6c549d253679aae9e2dcff59
TechnoSavage <technosavage@gmail.com>
2022-01-18 15:03:08 -0500

    .

--- Custos/c2_concept.py
+++ Custos/c2_concept.py
@@ -6,28 +6,38 @@ import subprocess
 from datetime import datetime
 
 #API to export PCAP for previous 24 hours
-pcap_file = 'athlab4.pcap'
+#use datetime to name present days PCAP
+#Use Custos API to generate PCAP file
+#Verify file exists
+#Create folder for all following commands
+
+pcap_file = 'athlab4.pcap' #Use datetime to name variable
 #Generate Zeek logs from PCAP, comment next line and uncomment the line after to use local Zeek config
-cmd = 'zeek -C -r ' + pcap_file + ' tcp_inactivity_timeout = 60min;'
-#cmd = 'zeek -C -r ' + pcap_file + ' local'
+cmd = 'zeek -C -r %s "tcp_inactivity_timeout = 60min;"' % pcap_file
+#cmd = 'zeek -C -r %s local' % pcap_file
 
 sp = subprocess.Popen(cmd, shell=True)
 rc = sp.wait()
 print(rc)
 
 #Create RITA database from Zeek logs
-rita_db_name = ''
-cmd = 'rita import ./*.log' + rita_db_name
-
+rita_db_name = 'th_concept'
+cmd = 'rita import ./*.log %s' % rita_db_name
+sp = subprocess.Popen(cmd, shell=True)
+rc = sp.wait()
+print(rc)
 
 #Execute RITA detection mothods and create dict object for each entry that meets score threshold
 
 confidence = 0.80  #Define confidence score threshold
 
 #RITA 'show-beacons' block
+cmd = 'rita show-beacons %s' % rita_db_name
+sp = subprocess.Popen(cmd, shell=True)
+rc = sp.wait()
+print(rc)
 
 #RITA 'show-beacons-fqdn' block
-
-
+cmd = 'rita show-beacons-fqdn %s' % rita_db_name
 
 
