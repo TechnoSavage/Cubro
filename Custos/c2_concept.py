00e953b3cf05ae0a020c5605a1ca18a231845c73
TechnoSavage <technosavage@gmail.com>
2022-01-18 16:55:24 -0500

    .

--- Custos/c2_concept.py
+++ Custos/c2_concept.py
@@ -13,44 +13,86 @@ from datetime import datetime
 
 pcap_file = 'athlab4.pcap' #Use datetime to name variable
 #Generate Zeek logs from PCAP, comment next line and uncomment the line after to use local Zeek config
-cmd = 'zeek -C -r %s "tcp_inactivity_timeout = 60min;"' % pcap_file
-#cmd = 'zeek -C -r %s local' % pcap_file
+try:
+    cmd = 'zeek -C -r %s "tcp_inactivity_timeout = 60min;"' % pcap_file
+    #cmd = 'zeek -C -r %s local' % pcap_file
 
-sp = subprocess.Popen(cmd, shell=True)
-rc = sp.wait()
-print(rc)
+    sp = subprocess.Popen(cmd, shell=True)
+    rc = sp.wait()
+except OSError as e:
+    print(e)
 
 #Create RITA database from Zeek logs
-rita_db_name = 'th_concept'
-cmd = 'rita import ./*.log %s' % rita_db_name
-sp = subprocess.Popen(cmd, shell=True)
-rc = sp.wait()
+try:
+    rita_db_name = 'th_concept'
+    cmd = 'rita import ./*.log %s' % rita_db_name
+    sp = subprocess.Popen(cmd, shell=True)
+    rc = sp.wait()
+except OSError as e:
+    print(e)
 
 #verify creation of RITA database
-cmd = 'rita list'
-sp = subprocess.Popen(cmd,
-                      stdout = subprocess.PIPE,
-                      stderr = subprocess.STDOUT)
-rc = sp.wait()
-output = sp.stdout.read()
-print(output.decode(encoding='utf-8'))
-
-#print(db_list)
-#if rita_db_name not in db_list:
-    #print('Failed to import Zeek logs to RITA')
-    #exit()
+try:
+    cmd = 'rita list'
+    sp = subprocess.Popen(cmd,
+                          shell = True,
+                          stdout = subprocess.PIPE,
+                          stderr = subprocess.STDOUT)
+    rc = sp.wait()
+    output = sp.stdout.read()
+except OSError as e:
+    print(e)
+db_list = output.decode(encoding='utf-8').splitlines()
+print(db_list)
+if rita_db_name not in db_list:
+    print('Failed to import Zeek logs to RITA')
+    exit()
 
 #Execute RITA detection mothods and create dict object for each entry that meets score threshold
 
 confidence = 0.80  #Define confidence score threshold
+detections = {}
 
 #RITA 'show-beacons' block
-cmd = 'rita show-beacons %s' % rita_db_name
-sp = subprocess.Popen(cmd, shell=True)
-rc = sp.wait()
-print(rc)
+try:
+    cmd = 'rita show-beacons %s' % rita_db_name
+    sp = subprocess.Popen(cmd, 
+                          shell=True,
+                          stdout = subprocess.PIPE,
+                          stderr = subprocess.STDOUT)
+    rc = sp.wait()
+    output = sp.stdout.read()
+except OSError as e:
+    print(e)
+beacons = output.decode(encoding='utf-8').splitlines()
+beacons = beacons.pop(0)
+hits = []
+for beacon in beacons:
+    fields = beacon.split(',')
+    score = float(fields[0])
+    if score >= confidence:
+        hits.append(beacon)
+detections[beacons] = hits
 
 #RITA 'show-beacons-fqdn' block
-cmd = 'rita show-beacons-fqdn %s' % rita_db_name
+try:
+    cmd = 'rita show-beacons-fqdn %s' % rita_db_name
+    sp = subprocess.Popen(cmd, 
+                          shell=True,
+                          stdout = subprocess.PIPE,
+                          stderr = subprocess.STDOUT)
+    rc = sp.wait()
+    output = sp.stdout.read()
+except OSError as e:
+    print(e)
+beacons_fqdn = output.decode(encoding='utf-8').splitlines()
+beacons_fqdn = beacons_fqdn.pop(0)
+hits = []
+for beacon in beacons_fqdn:
+    fields = beacon.split(',')
+    score = float(fields[0])
+    if score >= confidence:
+        hits.append(beacon)
+detections[beacons_fqdn] = hits
 
-
+print(detections)
